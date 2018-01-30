4fba00988f2cadf9c7124fa528706c9182bf143f
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-01-30 12:47:18 -0500

    fixed shit w the mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,9 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       name = params[:contact][:name]
-      email = params[:content][:email]
-      body = params[:content][:comments]
-      Contact.contact_email(name, email, body).deliver
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      ContactMailer.contact_email(name, email, body).deliver
       flash[:success] = "Thanks! We'll get back to you soon!"
       redirect_to new_contact_path
     else
