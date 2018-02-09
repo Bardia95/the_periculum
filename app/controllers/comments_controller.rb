82eea77ea783228f4d4a068f7a430b41837f0804
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-02-08 21:33:44 -0500

    0.1.7

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,7 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment = Comment.find(params[:id])
+    @comment.user = current_user
     @comment.destroy
     redirect_back(fallback_location: root_path)
   end
